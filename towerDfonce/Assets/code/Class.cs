195851fd62dbbc231a1f974111bfb7cb45d12a9a
TortueBleu <klarir.games@gmail.com>
2021-01-24 18:14:05 +0100

    ajout des classes

---
+++ towerDfonce/Assets/code/Class.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+private class tour {
+    int attaqueSpeed  ;
+    int degats ;
+    int range ;
+    int respawnTime;
+}
+private class monstre{
+    int moveSpeed;
+    int degats;
+    int vie;
+    int attaqueSpeed;
+}   
+private class sort{
+    int coutEnMana;
+    int CoolDown;
+}
