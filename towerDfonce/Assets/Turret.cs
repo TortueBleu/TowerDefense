0c9d41b57f1849c3859af7d28eba9f430bb515fc
TortueBleu <klarir.games@gmail.com>
2021-01-24 18:10:11 +0100

    creation du repot git

---
+++ towerDfonce/Assets/Turret.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public Transform target;
+    public float range;
+    public float attack_speed;
+
+    public string enemyTag = "Enemy";
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+     void UpdateTarget()
+    {
+        GameObject[] ennemis = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in ennemis)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+
+    //afficher la range de la tourelle
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null)
+        {
+            return;
+        }
+        Vector3 dir = target.position - transform.position;
+        Debug.DrawRay(transform.position, dir, Color.red);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
