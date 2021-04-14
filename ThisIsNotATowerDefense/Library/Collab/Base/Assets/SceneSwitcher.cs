4d43cb02d1b85780688f5f9d9c8842382a4b5ab2
TortueBleu <klarir.games@gmail.com>
2021-04-14 15:05:19 +0200

    premier push

---
+++ ThisIsNotATowerDefense/Library/Collab/Base/Assets/SceneSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoStartScene()
+    {
+        SceneManager.LoadScene("start");
+        
+    }
+
+    public void GotoGameScene()
+    {
+        SceneManager.LoadScene("main");
+        Time.timeScale = 1;
+    }
+
+    public void GotoExitScene()
+    {
+        Application.Quit();
+        
+    }
+}
---
+++ ThisIsNotATowerDefense/Library/Collab/Download/Assets/SceneSwitcher.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+    public void GamePause()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void GotoStartScene()
+    {
+        SceneManager.LoadScene("start");
+        
+    }
+
+    public void GotoGameScene()
+    {
+        SceneManager.LoadScene("lvlchoose");
+        Time.timeScale = 1;
+    }
+
+    public void GotoExitScene()
+    {
+        Application.Quit();
+        
+    }
+
+    public void GotoLevel1()
+    {
+        SceneManager.LoadScene("main");
+        Time.timeScale = 1;
+    }
+
+    public void GotoLevel2()
+    {
+        SceneManager.LoadScene("lvl2");
+        Time.timeScale = 1;
+    }
+
+    public void GotoLevel3()
+    {
+        SceneManager.LoadScene("lvl3");
+        Time.timeScale = 1;
+    }
+
+    public void GotoLevel4()
+    {
+        SceneManager.LoadScene("lvl4");
+        Time.timeScale = 1;
+    }
+}
